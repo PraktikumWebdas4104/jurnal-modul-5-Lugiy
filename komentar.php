6f0df6b10c3c4a9455fb97412631f94618b706e2
Ugiy <43519843+Lugiy@users.noreply.github.com>
2018-10-05 10:25:30 +0700

    komentar
    lugi yunus
    6701170098
    d3mi 4104

--- komentar.php
+++ komentar.php
@@ -1,3 +1,16 @@
-<?php
+<form>
+	komentar : <input type="text" name="komentar">
+	<input type="submit" name="submit" value="Kirim">
+</form>
 
-?>
+<?php 
+if (isset($_POST['submit'])) {
+	$komentar=$$_POST['komentar'];
+
+	if(str_word_count($komentar)>=5){
+		echo $komentar;
+	}elseif (str_word_count($komentar)<5) {
+		echo"error, Kurang kata 1-5"
+	}
+}
+ ?>
