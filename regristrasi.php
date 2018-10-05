fe3b5d303de383c230ab6bc44bd84a4c248f4f1f
Ugiy <43519843+Lugiy@users.noreply.github.com>
2018-10-05 10:12:24 +0700

    registrasi
    Lugi Yunus
    6701170098
    D3MI 4104

--- regristrasi.php
+++ regristrasi.php
@@ -1,11 +1,11 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Form Registrasi</title>
-</head>
-<body>
+<form method="POST">
+	<table>
+		<tr><td>NIM</td><td> : <input name="nim" type="number" length="10"></td></tr>
 
+ 		<tr><td>Nama</td><td> : <input name="nama" type="text" length="25"></td></tr>
 
+		<tr><td>Email</td><td> : <input type="email" name="email"></td></tr>
 
-</body>
-</html>
+		<tr><td colspan="2" align="right"><input value="Daftar" type="submit">		
+	</table>
+</form>
